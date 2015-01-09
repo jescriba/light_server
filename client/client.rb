cf2a6d0ce9b5cef099079fbe274512103e43a3f2
jescriba <escribirajoshua@gmail.com>
2015-01-08 22:46:35 -0800

    Made client do random colors.

--- client/client.rb
+++ client/client.rb
@@ -2,9 +2,15 @@
 require 'httparty'
 require 'json'
 
-puts "Assuming mode is fill"
-puts "r, g, b"
-red, green, blue = gets.chomp.split(",").map { |el| el.to_i }
-header = { 'Content-Type' => 'application/json' }
-body = { "status" => "on", "mode" => "fill", "color" => { "red" => red, "blue" => blue, "green" => green } }.to_json
-HTTParty.post("http://0.0.0.0:4567/request", { :body => body, :headers => header })
+#puts "Assuming mode is fill"
+#puts "r, g, b"
+#red, green, blue = gets.chomp.split(",").map { |el| el.to_i }
+while true
+  red = rand(128) + 128
+  blue = rand(128) + 128
+  green = rand(128) + 128
+  header = { 'Content-Type' => 'application/json' }
+  body = { "status" => "on", "mode" => "fill", "color" => { "red" => red, "blue" => blue, "green" => green } }.to_json
+  HTTParty.post("http://10.0.0.14:4567/request", { :body => body, :headers => header })
+  sleep(0.1)
+end
